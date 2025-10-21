86e38bdf0e079323865b2ff645ed9ceefe37689b
Onur Ünal <74546805+onurunaall@users.noreply.github.com>
2025-10-21 18:18:49 +0200

    Add ModelBuilder class for model creation
    Implemented ModelBuilder class to create models based on specified architectures.

---
+++ models/model_builder.py
@@ -0,0 +1,51 @@
+from typing import Optional
+from tensorflow import keras
+from .vgg16_model import VGG16Model
+from .base_model import BaseModel
+
+class ModelBuilder:
+    """Factory for creating models"""
+    
+    SUPPORTED_ARCHITECTURES = {
+        'vgg16': VGG16Model,
+        # 'resnet50': ResNet50Model,
+        # 'efficientnet': EfficientNetModel,
+    }
+    
+    @classmethod
+    def create_model(cls,
+                     architecture: str = 'vgg16',
+                     **kwargs) -> BaseModel:
+        """
+        Create model based on architecture name
+        
+        Args:
+            architecture: Model architecture name
+            **kwargs: Arguments to pass to model constructor
+            
+        Returns:
+            Initialized model instance
+        """
+        if architecture not in cls.SUPPORTED_ARCHITECTURES:
+            raise ValueError(
+                f"Unknown architecture: {architecture}. "
+                f"Supported: {list(cls.SUPPORTED_ARCHITECTURES.keys())}"
+            )
+        
+        model_class = cls.SUPPORTED_ARCHITECTURES[architecture]
+        return model_class(**kwargs)
+    
+    @classmethod
+    def from_config(cls, config) -> BaseModel:
+        """Create model from configuration object"""
+        return cls.create_model(
+            architecture=config.model.architecture,
+            input_shape=(config.data.image_size[0], 
+                        config.data.image_size[1], 
+                        config.data.channels),
+            num_classes=config.model.num_classes,
+            pretrained=config.model.pretrained,
+            freeze_base=config.model.freeze_base,
+            unfreeze_layers=config.model.unfreeze_layers,
+            dropout_rate=config.model.dropout_rate
+        )
